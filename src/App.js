65a76c4644e5537969552bd8f2a27764bbe6200c
Snow0wl <des00012@mix.wvu.edu>
2023-11-02 17:02:02 -0400

    commit lab7

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 import Navbar from './Navbar';
 import Card from './Card';
 import Home from './Home';
+import Contact from './Contact';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      <Card />
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="card" element={<Card />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Contact() {
+    return(
+        <form>
+            <label for="form1">Form</label>
+            <input type="data" class="form-control" id="form1" placeholder="Enter data"></input>
+        </form>
+    );
+}
+export default Contact;
